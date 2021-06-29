def4a66468bd6e877b1de071e74cd744313949ca
Akelly <akelly982@hotmail.com>
2021-06-29 13:53:22 +1000

    finished with getting java to startup , started with Python
    didn't get all the way through python due to some form of incompatibility issue where some of the pyDev forms / template modules where not showing up. tried newer and older version which should of worked but could not get it to happen

--- exampleJavaFx/src/application/Main.java
+++ exampleJavaFx/src/application/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setTitle("Hello World");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- exampleJavaFx/src/application/SampleController.java
+++ exampleJavaFx/src/application/SampleController.java
@@ -1,5 +1,15 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class SampleController {
+	
+	@FXML
+	private Label label;
+	
+	public void initialize() {
+		label.setText("hello world");
+	}
 	
 }
